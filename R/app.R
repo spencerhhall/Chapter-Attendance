0853aed5a90e9e542b92351e470353d854574c6d
spencerhh <spencer.hall16@gmail.com>
2019-02-27 19:32:46 -0800

    Added code to clean csv file.

---
+++ R/app.R
@@ -0,0 +1,12 @@
+# Load necessary packages
+library("shiny")
+library("ggplot2")
+library("dplyr")
+
+ui <- fluidPage()
+
+server <- function(input, output) {
+
+}
+
+shinyApp(ui = ui, server = server)
---
+++ R/clean_data.R
@@ -0,0 +1,17 @@
+# This file will be used to clean the raw .csv file
+
+# Load necessary packages
+library("dplyr")
+library("stringr")
+
+attendance_data <- read.csv("../data/W19 - W19.csv", stringsAsFactors = FALSE)
+
+# Isolates the second to last 3 rows containing the numerical data for each week
+attendance_data <- filter(attendance_data, Week.1..1.7. != 'x') %>%
+  filter(Last.Name != "Sum Check") %>%
+  select(Week.1..1.7.:Week.10..3.11.)
+
+rownames(attendance_data) <- c("Absences", "Excused", "Present")
+
+colnames(attendance_data) <- c("Week 1", "Week 2", "Week 3", "Week 4", "Week 5",
+                               "Week 6", "Week 7", "Week 8", "Week 9", "Week 10")
