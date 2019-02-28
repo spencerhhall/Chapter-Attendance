23c24f9749527d4420de7f52cf97a7e272c44e3c
spencerhh <spencer.hall16@gmail.com>
2019-02-27 20:07:38 -0800

    Reformatted output of cleaning file. Trying to implement plotting.

--- R/app.R
+++ R/app.R
@@ -1,14 +1,31 @@
 # Load necessary packages
 library("shiny")
 library("ggplot2")
-library("dplyr")
 
 ui <- fluidPage(
+  titlePanel("Weekly Chapter Attendance Data"),
   
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "week", label = "Week",
+                  min = 1, max = 10, value = 1)
+    ),
+    mainPanel(
+      plotOutput(outputId = "plot")
+    )
+  )
 )
 
 server <- function(input, output) {
-
+  output$plot <- renderPlot({
+    df <-  read.csv("../data/clean_W19.csv", stringsAsFactors = FALSE)
+    df <- df[input$week:input$week + 2]
+    View(df)
+    my_plot <- ggplot(data = df) +
+      geom_bar(mapping = aes(x = Last.Name), width = 1)
+    my_plot + coord_polar()
+    my_plot
+  })
 }
 
 shinyApp(ui = ui, server = server)
--- R/clean_data.R
+++ R/clean_data.R
@@ -2,17 +2,21 @@
 
 # Load necessary packages
 library("dplyr")
+library("tidyr")
 
 attendance_data <- read.csv("../data/W19 - W19.csv", stringsAsFactors = FALSE)
 
 # Isolates the second to last 3 rows containing the numerical data for each week
 attendance_data <- filter(attendance_data, Week.1..1.7. != 'x') %>%
   filter(Last.Name != "Sum Check") %>%
-  select(Week.1..1.7.:Week.10..3.11.)
+  select(Last.Name:Week.10..3.11.) %>%
+  gather(key = week, value = frequency, -Last.Name)
 
 rownames(attendance_data) <- c("Absences", "Excused", "Present")
 
 colnames(attendance_data) <- c("Week 1", "Week 2", "Week 3", "Week 4", "Week 5",
                                "Week 6", "Week 7", "Week 8", "Week 9", "Week 10")
 
-write.csv(attendance_data, "clean_W19.csv", row.names = FALSE)
+
+
+write.csv(attendance_data, "../data/clean_W19.csv", row.names = TRUE)
